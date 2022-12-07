24a55549d3902803d6a540643bc40f13f153207f
efforthye <efforthye@gmail.com>
2022-12-08 02:03:16 +0900

    테스트 데이터베이스 추가 시작

--- node-express-server/models/index.js
+++ node-express-server/models/index.js
@@ -2,13 +2,22 @@
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+
+// database
+const TestInfo = require("./test");
+const db = {TestInfo};
+
 let sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+// database
+TestInfo.init(sequelize);
+
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 module.exports = db;
