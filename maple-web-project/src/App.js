10e6ed83267f180b600c3800ec92e20599a6e7d5
efforthye <efforthye@gmail.com>
2022-12-08 22:00:37 +0900

    현재 url 띄우기

--- maple-web-project/src/App.js
+++ maple-web-project/src/App.js
@@ -2,9 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+
   return (
     <div className="App">
-
+      <div onClick={()=>{
+        window.prompt("이 글의 트랙백 주소입니다. Ctrl+C를 눌러 클립보드로 복사하세요", window.location.href);
+      }}>클릭시 현재 url 띄움</div>
       
     </div>
   );
